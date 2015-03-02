61f4bf33dbae434dc1a267da7b01d613564567f3
Fredrik Hagman <hagmanen@gmail.com>
2015-03-02 14:57:13 +0100

    Replaced index.html with index.ejs and introduced ejs

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express    = require('express'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    ejs        = require('ejs');
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,7 +12,14 @@ router.get('/', function(req, res) {
 	res.json({message: 'some silly message'});
 });
 
+app.set('views', __dirname + '/root');
+app.set('view engine', 'ejs');
+app.engine('html', ejs.renderFile);
+
 app.use('/api', router);
-app.use('/', express.static(__dirname + '/root'));
+
+app.get('/', function(req, res) {
+	res.render(__dirname + '/root');
+});
 
 app.listen(8080);
