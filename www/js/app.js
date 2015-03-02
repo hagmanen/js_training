407200cda6aa27d2697294418f0716e45298ef22
Fredrik Hagman <hagmanen@gmail.com>
2015-03-02 17:27:25 +0100

    Added backbone.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,10 @@
-
 requirejs.config({
-    "baseUrl": "js/lib",
+    "baseUrl": "js/libs",
     "paths": {
       "app": "../app",
-      "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min"
+      "jquery": "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min",
+      "underscore": "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.2/underscore-min",
+      "backbone": "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min"
     }
 });
 
--- www/js/app/main.js
+++ www/js/app/main.js
@@ -1,5 +1,19 @@
-define(["jquery"], function($) {
-    $(function() {
-        $('body').append('<p>Im using jquery!</p>');
-    });
+define([
+	"jquery",
+	"underscore",
+	"backbone",
+], function($, _, Backbone) {
+	var Router = Backbone.Router.extend({
+		routes: {
+			'': 'home'
+		}
+	});
+
+	var router = new Router();
+	router.on('route:home', function() {
+		console.log('Here I am');
+		$('body').append('<p>Im using jquery!</p>');
+	});
+
+	Backbone.history.start();
 });
