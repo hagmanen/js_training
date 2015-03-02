4d906e71a536081a78ca25201120afe80f6793ad
Fredrik Hagman <hagmanen@gmail.com>
2015-03-02 14:33:56 +0100

    Added router for rest api

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
-var express = require('express');
+var express    = require('express'),
+    bodyParser = require('body-parser');
 var app = express();
 
-app.use("/", express.static(__dirname + "/root"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+	res.json({message: 'some silly message'});
+});
+
+app.use('/api', router);
+app.use('/', express.static(__dirname + '/root'));
 
 app.listen(8080);
