116ff387dfea91fdf5d5256b91fc53d0124d7f61
Fredrik Hagman <hagmanen@gmail.com>
2015-03-03 11:13:16 +0100

    Join POST /users

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ router.get('/', function(req, res) {
 	res.json({message: 'some silly message'});
 });
 
+app.post('/users', function(req, res) {
+	return res.send({'id': 'nrone'});
+});
+
 app.use('/api', router);
 app.use('/', express.static(__dirname + '/www'));
 
--- www/js/app/main.js
+++ www/js/app/main.js
@@ -9,17 +9,36 @@ define([
 			'': 'home'
 		}
 	});
+	var router = new Router();
+
+	var User = Backbone.Model.extend({
+		urlRoot: '/users'
+	});
 
 	var Join = Backbone.View.extend({
 		el: '.page',
 		render: function() {
 			var template = _.template(JoinTemplate, {});
 			this.$el.html(template);
+		},
+		events: {
+			'click .join': 'joinGame',
+			'click .watch': 'watchGame'
+		},
+		joinGame: function() {
+			var user = new User();
+			user.save({}, {
+				success: function(user) {
+					console.log(user);
+				}
+			});
+		},
+		watchGame: function() {
+			router.navigate('board', {trigger:true});
 		}
 	});
 	var join = new Join();
 
-	var router = new Router();
 	router.on('route:home', function() {
 		join.render();
 	});
