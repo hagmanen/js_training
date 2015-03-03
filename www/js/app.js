703a7e7c3c6ed41b0102699c22657acdd5e84453
Fredrik Hagman <hagmanen@gmail.com>
2015-03-03 10:21:37 +0100

    Join template added

--- www/js/app.js
+++ www/js/app.js
@@ -2,9 +2,11 @@ requirejs.config({
     "baseUrl": "js/libs",
     "paths": {
       "app": "../app",
+      "templates": "../../templates",
       "jquery": "//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min",
       "underscore": "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.2/underscore-min",
-      "backbone": "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min"
+      "backbone": "//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min",
+      "text": "//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text"
     }
 });
 
--- www/js/app/main.js
+++ www/js/app/main.js
@@ -2,17 +2,26 @@ define([
 	"jquery",
 	"underscore",
 	"backbone",
-], function($, _, Backbone) {
+	"text!templates/join.html"
+], function($, _, Backbone, JoinTemplate) {
 	var Router = Backbone.Router.extend({
 		routes: {
 			'': 'home'
 		}
 	});
 
+	var Join = Backbone.View.extend({
+		el: '.page',
+		render: function() {
+			var template = _.template(JoinTemplate, {});
+			this.$el.html(template);
+		}
+	});
+	var join = new Join();
+
 	var router = new Router();
 	router.on('route:home', function() {
-		console.log('Here I am');
-		$('body').append('<p>Im using jquery!</p>');
+		join.render();
 	});
 
 	Backbone.history.start();
